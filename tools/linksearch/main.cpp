6b9f5e44b7f8bc06a8b9a5619128526760e726d2
Hazzoz <hwils2019@gmail.com>
2024-01-13 16:27:56 +1100

    Set up initial command input

--- tools/linksearch/main.cpp
+++ tools/linksearch/main.cpp
@@ -1,6 +1,70 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static
+int
+process(
+	char const*	search_dir
+);
+
 int main(int argc, char* argv[]) {
 
-	return 0;
-	
+	char const* const	program_name	=	argv[0];
+	char const*			search_dir		=	nullptr;
+
+	switch (argc)
+	{
+	case 1:
+		fprintf(
+			stderr
+		,	"%s: search-directory not specified; use --help for usage\n"
+		,	program_name
+		);
+
+		return EXIT_FAILURE;
+	case 2:
+
+		if (0 == strcmp("--help", argv[1]))
+		{
+			printf(
+				"USAGE: %s { <search-directory> | --help }\n"
+			,	program_name
+			);	
+
+			return EXIT_SUCCESS;
+		}
+		else
+		{
+			search_dir = argv[1];
+		}
+
+		break;
+	default:
+		fprintf(
+			stderr
+		,	"%s: too many arguments; use --help for usage\n"
+		,	program_name
+		);
+
+		return EXIT_FAILURE;
+	}
+
+	return process(
+		search_dir
+	);
 }
+
+static
+int
+process(
+	char const*	search_dir
+)
+{
+	printf("process(search_dir=%s)\n", search_dir);
+
+	return 0;
+}
+
+
